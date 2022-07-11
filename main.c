389918d59d390a2ab30ead917c64c3daf1eb430e
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-07-11 23:09:20 +0100

    Remove all library dependencies

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <stdint.h>
+#define RW_reg  unsigned int
+#define RO_reg  const unsigned int
 
 #define GPIO_BASE_ADDR  0x50000000U
 
@@ -7,16 +8,16 @@
 
 typedef struct 
 {
-    uint32_t PINCNF_DIR      : 1;    // Pin direction: input (0) or output (1)
-    uint32_t PINCNF_INPUT    : 1;    // Connect (0) or disconnect (1) input buffer
-    uint32_t PINCNF_PULL     : 2;    // No pull (0) / pull down (1) / pull up (3)
-    uint32_t                 : 4;
-    uint32_t PINCNF_DRIVE    : 3;    // Drive config
-    uint32_t                 : 0;
-    uint32_t PINCNF_SENSE    : 2;    // Disable (0) / High (2) / Low (3)
+    RW_reg  PINCNF_DIR      : 1;    // Pin direction: input (0) or output (1)
+    RW_reg  PINCNF_INPUT    : 1;    // Connect (0) or disconnect (1) input buffer
+    RW_reg  PINCNF_PULL     : 2;    // No pull (0) / pull down (1) / pull up (3)
+    RO_reg                  : 4;
+    RW_reg  PINCNF_DRIVE    : 3;    // Drive config
+    RO_reg                  : 0;
+    RW_reg  PINCNF_SENSE    : 2;    // Disable (0) / High (2) / Low (3)
 } tPincnf_reg;
 
-#define OUT      (*((volatile uint32_t *) OUT_ADDR))
+#define OUT      (*((volatile RW_reg *) OUT_ADDR))
 #define PIN17_CNF   (*((volatile tPincnf_reg *) PIN17_CNF_ADDR))
 
 void main( void )
