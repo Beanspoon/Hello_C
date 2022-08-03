71931409a7612abdc85472d6934c5f58ae636653
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-08-04 00:13:02 +0100

    20220804 Begin implementation of systick

--- include/core_m4.h
+++ include/core_m4.h
@@ -4,6 +4,38 @@
 #include "types.h"
 
 /************************************************
+ * SYSTICK
+ * *********************************************/
+
+#define CORE_SYSTICK_BASE_ADDR  0xE000E010
+
+/**
+ * @brief Systick control and status register
+ * 
+ */
+typedef struct
+{
+    RW_reg  CSR_SYST_EN     : 1;    // Bit[0] Status of the counter; 0: disabled, 1: enabled
+    RW_reg  CSR_TICKINT     : 1;    // Bit[1] Interrupt status on counter=0; 0: disabled, 1: enabled
+    RW_reg  CSR_CLKSOURCE   : 1;    // Bit[2] Systick clock source; 0: external, 1: processor
+    const uint16_t          : 0;
+    RW_reg  CSR_COUNTFLAG   : 1;    // Bit[16] Indicates if counter has reached 0 since last read of this register; 0: false, 1: true
+    RW_reg                  : 0;
+} tSystick_csrReg;
+
+/**
+ * @brief Systick register map
+ * 
+ */
+typedef struct
+{
+    tSystick_csrReg SYST_CSR;   // 0x00 Control and status register
+    RW_reg          SYST_RVR;   // 0x04 Reload value register
+    RW_reg          SYST_CVR;   // 0x08 Current value register
+    RO_reg          SYST_CALIB; // 0x0C Calibration value register
+} tCore_systickRegmap;
+
+/************************************************
  * NVIC
  * *********************************************/
 
