f5147e5c70b1f6e7992681dff45c74d7bb8251e1
Beanspoon <37507920+Beanspoon@users.noreply.github.com>
2022-08-09 18:41:39 +0100

    20220804 implement systick (#1)
    * 20220804 Begin implementation of systick
    
    * 20220806 Begin writing systickInit()
    
    * 20220806 Calculate systick frequency coefficient
    
    * 20220807 Continue implementing systick
    
    * 20220807 Implement systick interrupt, init and busyWait
    
    * Systick implemented
    
    Co-authored-by: Pete <beanspoon@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,11 +1,24 @@
+#include "core_m4.h"
+
 #include "types.h"
+
+#include "config.h"
 
 #define GPIO_BASE_ADDR  0x50000000U
 
 #define OUT_ADDR         (GPIO_BASE_ADDR + 0x504)
+#define OUTSET_ADDR         (GPIO_BASE_ADDR + 0x508)
+#define OUTCLR_ADDR         (GPIO_BASE_ADDR + 0x50C)
 #define PIN17_CNF_ADDR      ( GPIO_BASE_ADDR + 0x744U )
 
-typedef struct 
+typedef struct
+{
+    uint32_t    tick_count;
+} tMain_context;
+
+static tMain_context* main_getContext( void );
+
+typedef struct
 {
     RW_reg  PINCNF_DIR      : 1;    // Pin direction: input (0) or output (1)
     RW_reg  PINCNF_INPUT    : 1;    // Connect (0) or disconnect (1) input buffer
@@ -17,17 +30,37 @@ typedef struct
 } tPincnf_reg;
 
 #define OUT      (*((volatile RW_reg *) OUT_ADDR))
+#define OUTSET      (*((volatile RW_reg *) OUTSET_ADDR))
+#define OUTCLR      (*((volatile RW_reg *) OUTCLR_ADDR))
 #define PIN17_CNF   (*((volatile tPincnf_reg *) PIN17_CNF_ADDR))
 
 void main( void )
 {
+    core_systickInit( config.systick_frequency, config.core_clock_frequency );
+
     PIN17_CNF.PINCNF_DIR = 1U;  // Output
     PIN17_CNF.PINCNF_INPUT = 1U;    // Disconnect
     PIN17_CNF.PINCNF_PULL = 0u; // No pull
     PIN17_CNF.PINCNF_DRIVE = 0u;    // Standard 0, standard 1
     PIN17_CNF.PINCNF_SENSE = 0u;    // Disable
 
-    OUT = 0u;
+    while(1)
+    {
+        OUTCLR = (1u << 17u);
+        core_busyWait( 1000u );
+        OUTSET = (1u << 17u);
+        core_busyWait( 1000u );
+    }
+}
 
-    while(1) {}
+void Systick_handler( void )
+{
+    tMain_context *pContext = main_getContext();
+    ++(pContext->tick_count);
+}
+
+static tMain_context* main_getContext( void )
+{
+    static tMain_context context = { 0 };
+    return &context;
 }
