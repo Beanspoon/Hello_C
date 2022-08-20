65a05cf0d6a896c22e5233b537f8b34c1a2f6377
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-08-20 16:29:21 +0100

    20220820 Begin radio definition

---
+++ src/hal_gpio.c
@@ -0,0 +1,15 @@
+#include "hal_gpio.h"
+
+tGpio_status gpio_configurePin( const uint8_t pin, const tGpio_pinConfig* const config )
+{
+    // Check for invalid parameter values
+    if( pin > GPIO_MAXPIN )
+    {
+        return GPIO_STATUS_INVALID;
+    }
+
+    if( GPIO_DIR_INPUT == config->pinConfig.DIR )
+    {
+        GPIO.PINCNF[pin]
+    }
+}
