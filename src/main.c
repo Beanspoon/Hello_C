8b0a4a9373ef19472dd0bc51a434a860174e8b7c
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-09-06 22:52:54 +0100

    20220906 Update main to match renamed functions

--- src/main.c
+++ src/main.c
@@ -12,10 +12,10 @@ void main( void )
     // Set up radio
 
     uint8_t packet[] = { 0x55, 0x55, 'H', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd', 0x55, 0x55 };
-    radio_setTxPacket( packet, sizeof(packet) );
+    radioCtrl_setTxPacket( packet, sizeof(packet) );
 
     while(1)
     {
-        
+
     }
 }
