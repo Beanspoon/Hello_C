ba3fe353fd7655644240b2efdc702fa29493e0eb
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-10-09 00:38:38 +0100

    Add button and LED definitions

--- common/include/config.h
+++ common/include/config.h
@@ -1,13 +1,19 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include "gpio.h"
+
 #include <stdint-gcc.h>
 
 // SYSTICK
 #define CONFIG_SYSTICK_FREQUENCY_HZ     1000u       // 1 kHz = 1 ms period
 #define CONFIG_CORE_CLOCK_FREQUENCY_HZ  64000000u   // 64 MHz
 
-// RADIO
-#define CONFIG_TRANSMIT_INTERVAL_MS    1000u
+//GPIO
+#define BTN0    GPIO_PIN13
+#define BTN1    GPIO_PIN14
+#define LED0    GPIO_PIN17
+#define LED1    GPIO_PIN18
+
 
 #endif // CONFIG_H
