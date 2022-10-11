aa877925a72cecb1f90aefdd88de6819128ebd40
Peter Kubiak <peter.s.kubiak@bath.edu>
2022-10-11 16:44:30 +0100

    Revert GPIO pin numbering change

--- chipsets/mbn52832/include/gpio.h
+++ chipsets/mbn52832/include/gpio.h
@@ -29,6 +29,7 @@ typedef enum
  */
 typedef enum
 {
+    GPIO_PIN0,
     GPIO_PIN1,
     GPIO_PIN2,
     GPIO_PIN3,
@@ -60,7 +61,6 @@ typedef enum
     GPIO_PIN29,
     GPIO_PIN30,
     GPIO_PIN31,
-    GPIO_PIN32,
     GPIO_PIN_MAX
 } tGpio_pin;
 
